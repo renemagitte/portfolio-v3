771c9fde8b71f750476a89392650988c342a4559
Ida Bergström <idalovisabergstrom@gmail.com>
2018-10-13 23:12:34 +0200

    about section closes with ctrl+c

---
+++ js/ctrlc.js
@@ -0,0 +1,27 @@
+
+(function(){
+
+  const pressed = [];
+  const commando = 'Controlc'
+
+  var _about = document.querySelector('.about');
+  var _aboutContent = document.querySelector('.about-content');
+
+
+  window.addEventListener('keyup', (e) => {
+    pressed.push(e.key);
+    pressed.splice(-commando.length -1, pressed.length - commando.length);
+
+    if(pressed.join('').includes(commando)){
+      _about.classList.remove('about-open');
+      _aboutContent.classList.remove('about-content-open');
+    }
+  })
+
+})();
+
+
+
+  
+
+
